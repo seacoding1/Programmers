c3eff87af6823001b1ea950e6631d6d48b92f150
Euna <110159365+seacoding1@users.noreply.github.com>
2023-05-17 02:21:27 +0900

    Update 푸드 파이트 대회.js

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,4 +1,18 @@
-function solution(food) {
-    var answer = '';
-    return answer;
-}
+var answer = '';
+        var stack = [];
+        for(var i = 1; i < food.length; i++){
+            for(var j = 1; j <= food[i]/2; j++){
+                answer += i;
+                stack.push(i);
+            }
+            if(i == food.length-1){
+                    answer += 0;
+                }
+
+        }
+
+        while(stack.length != 0){
+            answer += stack.pop();
+        }
+
+        return answer;

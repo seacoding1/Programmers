12bd6e767a103ac42c602401e1f94e74a347b903
Euna <110159365+seacoding1@users.noreply.github.com>
2023-06-06 23:21:46 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,12 @@
+function solution(s) {
+     var answer = '';
+            var arr = s.split(' ').map(Number);
+            
+            arr.sort(function (a, b){
+                return a - b;
+            });
+
+            answer += arr[0] + ' ' + arr[arr.length-1];
+
+            return answer;
+}
